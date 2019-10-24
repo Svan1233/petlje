2b05f3a5fe846953e823326de06c7f5deb6925a5
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2019-10-24 11:20:57 +0200

    Add files via upload

---
+++ svanTipuricPetlje/svanTipuricPetlje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst:");
+            unosTeksta = Console.ReadLine();
+            Console.WriteLine(unosTeksta);
+            for (int i = 0; i < unosTeksta.Length; i++)//Prolazi kroz petlju toliko puta koliko unosTeksta ima slova
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i + 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
